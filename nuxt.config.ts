c9f271c6fb32a1155820319c25988d62ec85fb7c
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-07-02 22:39:35 +0200

    feat: add firebase plugin and nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  typescript: {
+    shim: false,
+  },
+  modules: [
+    '@vueuse/nuxt',
+  ],
+  runtimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    }
+  },
 })
