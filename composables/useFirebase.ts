4a3d8306dfff8d4b771eefdbce74ac39ce52d2f3
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-07-03 20:15:07 +0200

    feat: add firebase auth functions

---
+++ composables/useFirebase.ts
@@ -0,0 +1,54 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth'
+
+export const createUser = async (email: string, password: string) => {
+  const auth = getAuth()
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+  })
+
+  return userCredential
+}
+
+export const signInUser = async (email: string, password: string) => {
+  const auth = getAuth()
+  const userCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+  })
+
+  return userCredential
+}
+
+export const initUser = async () => {
+  const auth = getAuth()
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid
+    } else {
+
+    }
+  })
+}
+
+export const signOutUser = async () => {
+  const auth = getAuth()
+  const result = await auth.signOut()
+
+  return result
+}
