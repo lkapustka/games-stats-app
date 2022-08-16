1e85b042455e4a4fe4904e5c6c42aba69e801b2e
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-08-16 21:43:19 +0200

    feat: add FormLogin component and update app.vue

---
+++ components/FormLogin.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import BaseInput from '~/components/UI/BaseInput.vue'
+
+const signInForm = ref({ email: '', password: '' })
+const signIn = () => {
+  signInUser(signInForm.value.email, signInForm.value.password)
+  signInForm.value = { email: '', password: '' }
+}
+</script>
+
+<template>
+  <form class="p-5 m-auto bg-white rounded-3" @submit.prevent>
+    <div class="mb-3">
+      <BaseInput
+        v-model="signInForm.email"
+        label="Email address"
+        type="email"
+        placeholder="Type your email"
+      />
+    </div>
+    <div class="mb-3">
+      <BaseInput
+        v-model="signInForm.password"
+        label="Password"
+        type="password"
+        placeholder="Type your password"
+      />
+    </div>
+    <button type="submit" class="btn btn-primary" @click="signIn">
+      Log In
+    </button>
+  </form>
+</template>
