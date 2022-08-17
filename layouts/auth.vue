c4efe6c913b3fc78fad53e88f25b685ce40dab00
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-08-17 18:41:42 +0200

    feat: add default and auth layouts

---
+++ layouts/auth.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid background">
+    <div class="col-12 col-sm-8 col-md-6 col-lg-4 p-3 shadow form-wrapper">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.background {
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  background-image: linear-gradient(
+    to right bottom,
+    #172a4d,
+    #0c295c,
+    #04276b,
+    #0c2378,
+    #1f1b83
+  );
+}
+.form-wrapper {
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 0.5rem;
+}
+</style>
