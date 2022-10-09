ad5e626d1849e3dbbd80a8902ad4b366f8034ad9
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-10-09 23:47:50 +0200

    refactor: remove import and div

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['bootstrap/dist/css/bootstrap.min.css'],
   typescript: {
