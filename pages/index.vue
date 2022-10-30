272e70caed6da69b15f1e4ce36d09040a1184de0
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-10-30 16:23:11 +0100

    refactor: remove links and change nuxt layout

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,7 @@ const signOut = async () => await signOutUser()
 </script>
 
 <template>
-  <NuxtLayout name="auth">
-    <button v-if="firebaseUser" class="btn btn-danger w-100" @click="signOut">
-      Sign Out
-    </button>
-    <div v-if="!firebaseUser" class="text-center">User is signed out</div>
-
-    <NuxtLink to="/login" class="btn btn-primary w-100 my-3">Log In</NuxtLink>
-    <NuxtLink to="/register" class="btn btn-primary w-100">Register</NuxtLink>
-  </NuxtLayout>
+  <NuxtLayout name="dashboard"> </NuxtLayout>
 </template>
 
 <style lang="scss" scoped></style>
